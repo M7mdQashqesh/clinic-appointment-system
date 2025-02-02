b5803c40fe2fa0fbb128b3a25ea05a42a9058b1f
Mohammed Qashqesh <mohammedbelal601@gmail.com>
2025-02-02 17:52:40 +0200

    complete login page design

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,13 +1,39 @@
 import "./loginPage.css";
+import EmailIcon from "@mui/icons-material/Email";
+import HttpsIcon from "@mui/icons-material/Https";
 
 const LoginPage = () => {
   return (
     <div className="login-page">
-      <div className="container">
-        
+      <div className="image">
+        <img src="/doctor-login.svg" alt="doctor" />
+      </div>
+      <div className="form">
+        <h1>Welcome!</h1>
+        <form>
+          <div className="email">
+            <EmailIcon />
+            <input
+              type="email"
+              name="email"
+              id="email"
+              placeholder="Your email"
+            />
+          </div>
+          <div className="password">
+            <HttpsIcon />
+            <input
+              type="password"
+              name="password"
+              id="password"
+              placeholder="Your password"
+            />
+          </div>
+          <input type="submit" value="Login" />
+        </form>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default LoginPage
+export default LoginPage;
