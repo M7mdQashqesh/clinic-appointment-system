73d977354142c9437e9c8bd86c20b3253cf0ff37
Mohammed Qashqesh <mohammedbelal601@gmail.com>
2025-02-02 16:43:59 +0200

    Initial creation for login page

---
+++ src/pages/LoginPage/LoginPage.tsx
@@ -0,0 +1,13 @@
+import "./loginPage.css";
+
+const LoginPage = () => {
+  return (
+    <div className="login-page">
+      <div className="container">
+        
+      </div>
+    </div>
+  )
+}
+
+export default LoginPage
