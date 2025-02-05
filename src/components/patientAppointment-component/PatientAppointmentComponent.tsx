7f90c3a49503c24e33faf9771347d03df2cfbb20
Mohammed Qashqesh <mohammedbelal601@gmail.com>
2025-02-06 00:15:21 +0200

    create Patient Appointment page

---
+++ src/components/patientAppointment-component/PatientAppointmentComponent.tsx
@@ -0,0 +1,59 @@
+import "./patientAppointmentComponent.css";
+
+const PatientAppointmentComponent = () => {
+  return (
+    <div className="patient-appointment-component">
+      <div className="container">
+        <h2>We'll call you back</h2>
+        <form>
+          <div className="name-contact">
+            <input
+              type="text"
+              name="patientName"
+              placeholder="Full name"
+              required
+            />
+            <input
+              type="number"
+              name="patientContact"
+              placeholder="Mobile number"
+              required
+            />
+          </div>
+          <div className="age-gender">
+            <input
+              type="number"
+              name="patientAge"
+              placeholder="Your age"
+              required
+            />
+            <select name="gender">
+              <option value="male">Male</option>
+              <option value="female">Female</option>
+            </select>
+          </div>
+          <div className="date-time">
+            <div className="date">
+              <label htmlFor="date">Date</label>
+              <input type="date" name="date" id="date" required />
+            </div>
+            <div className="time">
+              <label htmlFor="time">Time</label>
+              <input type="time" name="time" id="time" required />
+            </div>
+          </div>
+          <div className="description">
+            <textarea
+              name="description"
+              placeholder="Symptoms description"
+              required
+            />
+          </div>
+          <input type="submit" value="Submit" />
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default PatientAppointmentComponent;
--- src/pages/patientAppointment/PatientAppointment.tsx
+++ src/pages/patientAppointment/PatientAppointment.tsx
@@ -1,12 +1,14 @@
 import HeaderComponent from "../../components/header-component/HeaderComponent";
+import PatientAppointmentComponent from "../../components/patientAppointment-component/patientAppointmentComponent";
 import "./patientAppointment.css";
 
 const PatientAppointment = () => {
   return (
     <div className="patient-appointment">
       <HeaderComponent />
+      <PatientAppointmentComponent />
     </div>
-  )
-}
+  );
+};
 
-export default PatientAppointment
+export default PatientAppointment;
